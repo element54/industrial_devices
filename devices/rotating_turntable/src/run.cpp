b930ccfdb200d6fc2f06d1420187cb520df3eb01
Torsten Jandt <element54@posteo.de>
2016-08-23 14:02:12 +0000

    updated rtt

--- devices/rotating_turntable/src/run.cpp
+++ devices/rotating_turntable/src/run.cpp
@@ -10,13 +10,15 @@
 using namespace std;
 using namespace PropertyServer;
 
+enum RUN_MODE{RUN_MODE_STOP, RUN_MODE_LEFT, RUN_MODE_RIGHT};
 
 const double one_step = 0.9;
-bool run = false;
 double ms_per_step = 0.0;
 
+enum RUN_MODE mode = RUN_MODE_STOP;
+
 int main() {
-    auto rtt = make_shared<Device>( "rtt", "RTT", "Rotating Turntable" );
+    auto rtt = make_shared<Device>( "rotating_turntable", "Rotating Turntable", "Rotating Turntable" );
     auto target_speed = make_shared<Property>( "target_speed", "Target speed", "deg/s", false, 0.0, -720.0, 720.0 );
     auto current_speed = make_shared<Property>( "current_speed", "Current speed", "deg/s", true, 0.0, -720.0, 720.0 );
     rtt->add_property( target_speed );
@@ -27,10 +29,14 @@ int main() {
                                 double speed = property->as_double();
                                 current_speed->set_double( speed );
                                 if( speed == 0.0 ) {
-                                    run = false;
+                                    mode = RUN_MODE_STOP;
                                 } else {
-                                    run = true;
-                                    double steps_per_second = speed / one_step;
+                                    if(speed > 0.0) {
+                                        mode = RUN_MODE_LEFT;
+                                    } else {
+                                        mode = RUN_MODE_RIGHT;
+                                    }
+                                    double steps_per_second = abs(speed) / one_step;
                                     double second_per_step = 1.0 / (steps_per_second);
                                     ms_per_step = (second_per_step * 1000.0);
                                 }
@@ -46,12 +52,20 @@ int main() {
        int states[][4] = {{1,0,1,0},{1,0,0,0},{1,0,0,1},{0,0,0,1},{0,1,0,1},{0,1,0,0},{0,1,1,0},{0,0,1,0}};
        int count = 0;
        for (;;) {
-        if(run) {
+        if(mode != RUN_MODE_STOP) {
             digitalWrite(COIL_1A, states[count][0]);
             digitalWrite(COIL_1B, states[count][1]);
             digitalWrite(COIL_2A, states[count][2]);
             digitalWrite(COIL_2B, states[count][3]);
-            count = (count + 1) % 8;
+            if(mode == RUN_MODE_LEFT) {
+                count = (count + 1) % 8;
+            } else {
+                if(count == 0) {
+                    count = 7;
+                } else {
+                    count--;
+                }
+            }
             delay (ms_per_step);
         } else {
             delay (100);
